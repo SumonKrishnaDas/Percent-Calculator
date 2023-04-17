d2d4d6b5720b18191a45ed53c65944f5bd5e92ea
SumonKrishnaDas <sumondash51583@gmail.com>
2023-04-17 18:28:21 +0600

    first commit

---
+++ discount.js
@@ -0,0 +1,26 @@
+document.getElementById('btn-calculate').addEventListener('click',discounted);
+
+function discounted(){
+
+
+
+
+const inputField= document.getElementById('input-field');
+const inputAmountString=inputField.value
+inputAmount=parseFloat(inputAmountString);
+inputField.value='';
+
+
+const percentField=document.getElementById('percent-field');
+const percentAmountString = percentField.value;
+const percentAmount=parseFloat(percentAmountString);
+const calculate= (percentAmount*inputAmount)/100;
+console.log(calculate);
+percentField.value=' '
+
+const setup = document.getElementById('result');
+setup.innerText=calculate;
+
+
+
+}
